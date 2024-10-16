47c2c35f3e4572d8ebf54264914a7ca5db8e32a6
Varun Nair <nairvarun104@gmail.com>
2024-10-17 02:52:30 +0530

    checkpoint

--- src/main.py
+++ src/main.py
@@ -1,53 +1,75 @@
 import asyncio
 import aiohttp
+import tomllib
+from pathlib import Path
+from dataclasses import dataclass
 
-class Leetcode():
-  def __init__(self):
-    self.url: str = "https://leetcode.com/graphql/"
-    self.query: str = """
-      query userPublicProfileAndProblemsSolved($username: String!) {
-        userContestRanking(username: $username) {
-          rating
-          globalRanking
-          topPercentage
+
+
+@dataclass
+class Config:
+  users: list[str]
+
+
+@dataclass(frozen=True)
+class Query:
+  url: str = "https://leetcode.com/graphql/"
+  __query: str = """
+    query userPublicProfileAndProblemsSolved($username: String!) {
+      userContestRanking(username: $username) {
+        rating
+        globalRanking
+        topPercentage
+      }
+      matchedUser(username: $username) {
+        profile {
+          ranking
         }
-        matchedUser(username: $username) {
-          profile {
-            ranking
-          }
-          submitStatsGlobal {
-            acSubmissionNum {
-              difficulty
-              count
-            }
+        submitStatsGlobal {
+          acSubmissionNum {
+            difficulty
+            count
           }
         }
       }
-    """
+    }
+  """
 
-  async def get_user_data(self, username: str):
-    json_data: dict = {
-      'query': self.query,
+  @classmethod
+  def get_query_data(cls, username):
+    return {
+      'query': cls.__query,
       'variables': {
         'username': username
       }
     }
-    async with aiohttp.ClientSession() as session:
-      async with session.post(self.url, json=json_data) as response:
-        # handle errors
-        # {'errors': [{'message': 'User matching query does not exist.', 'locations': [{'line': 3, 'column': 17}], 'path': ['userContestRanking']}, {'message': 'That user does not exist.', 'locations': [{'line': 8, 'column': 17}], 'path': ['matchedUser'], 'extensions': {'handled': True}}], 'data': {'userContestRanking': None, 'matchedUser': None}}
-        if response.status == 200:
-          data = await response.json()
-          print(data)
-        else:
-          print(response.status)
 
-class Configuration():
-  ...
+
+def get_configuration() -> Config:
+  configuration_file: Path = Path.joinpath(Path.home(), '.config', 'leetstalker', 'config.toml')
+  with configuration_file.open(mode='rb') as f:
+    configuration: dict[str, list[str]] = tomllib.load(f)
+  return Config(**configuration)
+
+
+async def get_data(session, user):
+    async with session.post(Query.url, json=Query.get_query_data(user)) as response:
+      # handle errors
+        # {'errors': [{'message': 'User matching query does not exist.', 'locations': [{'line': 3, 'column': 17}], 'path': ['userContestRanking']}, {'message': 'That user does not exist.', 'locations': [{'line': 8, 'column': 17}], 'path': ['matchedUser'], 'extensions': {'handled': True}}], 'data': {'userContestRanking': None, 'matchedUser': None}}
+      if response.status == 200:
+        data = await response.json()
+        return data
+      else:
+        print(response.status)
 
 async def main() -> int:
-  lc = Leetcode()
-  await lc.get_user_data('gultandon')
+  config: Config = get_configuration()
+  async with aiohttp.ClientSession() as session:
+    tasks = (get_data(session, user) for user in config.users)
+
+    responses = await asyncio.gather(*tasks)
+    for response in responses:
+      print(response)
   return 0
 
 if __name__ == "__main__":
