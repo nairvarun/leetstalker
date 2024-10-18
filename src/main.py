df7647173892e02ec24f826632a7f0f6bccbcc16
Varun Nair <nairvarun104@gmail.com>
2024-10-18 11:39:44 +0530

    add tables

--- src/main.py
+++ src/main.py
@@ -3,7 +3,9 @@ import aiohttp
 import tomllib
 from pathlib import Path
 from dataclasses import dataclass
-
+from heapq import heapify, heappop
+from rich.console import Console
+from rich.table import Table
 
 
 @dataclass
@@ -45,6 +47,42 @@ class Query:
     }
 
 
+@dataclass(order=True)
+class User:
+  ranking: int  # this will determine the order
+  username: str
+
+  # question metrics
+  questions_solved: int
+  hard_questions_solved: int
+  medium_questions_solved: int
+  easy_questions_solved: int
+
+  # contest metrics
+  contest_rating: float | None
+  contest_ranking: int | None
+  contest_percentile: float | None
+
+  def __init__(self, username, data):
+    self.username = username
+    data = data['data']
+    if data['userContestRanking'] is not None:
+      self.contest_rating = data['userContestRanking']['rating']
+      self.contest_ranking = data['userContestRanking']['globalRanking']
+      self.contest_percentile = data['userContestRanking']['topPercentage']
+    else:
+      self.contest_rating = None
+      self.contest_ranking = None
+      self.contest_percentile = None
+
+    # TODO: handle usr not exists err
+    self.ranking = data['matchedUser']['profile']['ranking']
+    self.questions_solved = data['matchedUser']['submitStatsGlobal']['acSubmissionNum'][0]['count']
+    self.easy_questions_solved = data['matchedUser']['submitStatsGlobal']['acSubmissionNum'][1]['count']
+    self.medium_questions_solved = data['matchedUser']['submitStatsGlobal']['acSubmissionNum'][2]['count']
+    self.hard_questions_solved = data['matchedUser']['submitStatsGlobal']['acSubmissionNum'][3]['count']
+
+
 def get_configuration() -> Config:
   configuration_file: Path = Path.joinpath(Path.home(), '.config', 'leetstalker', 'config.toml')
   with configuration_file.open(mode='rb') as f:
@@ -52,24 +90,56 @@ def get_configuration() -> Config:
   return Config(**configuration)
 
 
-async def get_data(session, user):
-    async with session.post(Query.url, json=Query.get_query_data(user)) as response:
+async def get_data(session, username) -> User:
+    async with session.post(Query.url, json=Query.get_query_data(username)) as response:
       # handle errors
         # {'errors': [{'message': 'User matching query does not exist.', 'locations': [{'line': 3, 'column': 17}], 'path': ['userContestRanking']}, {'message': 'That user does not exist.', 'locations': [{'line': 8, 'column': 17}], 'path': ['matchedUser'], 'extensions': {'handled': True}}], 'data': {'userContestRanking': None, 'matchedUser': None}}
       if response.status == 200:
         data = await response.json()
-        return data
+        if 'errors' not in data:
+          return User(username, data)
+        else:
+          # TODO: handle gracefully
+          print(data['errors'])
       else:
+        # TODO: handle gracefully
         print(response.status)
 
 async def main() -> int:
   config: Config = get_configuration()
   async with aiohttp.ClientSession() as session:
-    tasks = (get_data(session, user) for user in config.users)
+    tasks = [get_data(session, user) for user in config.users]
 
     responses = await asyncio.gather(*tasks)
-    for response in responses:
-      print(response)
+    heapify(responses)
+
+    table = Table()
+
+    table.add_column("Username", style="white")
+    table.add_column("Ranking", style="magenta")
+    table.add_column("Questions", style="blue")
+    table.add_column("Hard", style="red")
+    table.add_column("Medium", style="yellow")
+    table.add_column("Easy", style="green")
+    table.add_column("Contest Ranking", style="magenta")
+    table.add_column("Contest Rating", style="blue")
+
+    while responses:
+      user: User = heappop(responses)
+      table.add_row(
+        user.username,
+        str(user.ranking),
+        str(user.questions_solved),
+        str(user.hard_questions_solved),
+        str(user.medium_questions_solved),
+        str(user.easy_questions_solved),
+        str(user.contest_ranking) if user.contest_ranking is not None else '-',
+        str(user.contest_rating) if user.contest_rating is not None else '-',
+      )
+
+    console = Console()
+    console.print(table)
+
   return 0
 
 if __name__ == "__main__":
